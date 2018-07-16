158c627de562bc4764864f6422932a0201639bb6
Kathan Cheema <kcheema@sky.com>
2018-07-16 20:41:56 +0100

    This fixes #6 by using document.getElementById instead of convoluted old method using individual SVGs. Also tidies deploy and package.json
    Signed-off-by: Kathan Cheema <kcheema@sky.com>

--- public/javascripts/pcScript.ts
+++ public/javascripts/pcScript.ts
@@ -1,40 +1,41 @@
 window.onload = () => {
 
-    MapHandler.applyCanvas(JH105Info, document.querySelector("#JH105"));
-    MapHandler.applyCanvas(JH103Info, document.querySelector("#JH103"));
-    MapHandler.applyCanvas(MorrisonInfo, document.querySelector("#Morrison"));
+    return Promise.all([
+        MapHandler.applyCanvas(JH105Info, document.querySelector("#JH105")),
+        MapHandler.applyCanvas(JH103Info, document.querySelector("#JH103")),
+        MapHandler.applyCanvas(MorrisonInfo, document.querySelector("#Morrison"))
+    ]).then(() => {
+        const listOfPCs: HTMLCollection = document.getElementsByClassName("pc");
+        const header: HTMLElement = document.getElementById("title");
+        const searchBar: HTMLInputElement = document.getElementById("search") as HTMLInputElement;
 
-    const listOfPCs: HTMLCollection = document.getElementsByClassName("pc");
-    const header: HTMLElement = document.getElementById("title");
-    const searchBar: HTMLInputElement = document.getElementById("search") as HTMLInputElement;
+        // Create status checker and update the statuses
+        const statusChecker = new StatusHandler(header, listOfPCs);
+        statusChecker.updateStatuses();
 
-    // Create status checker and update the statuses
-    const statusChecker = new StatusHandler(header, listOfPCs);
-    statusChecker.updateStatuses();
+        // Create search handler for pc list and assign to the search bar
+        SearchHandler.assignSearchHandler(searchBar, listOfPCs);
 
-    // Create search handler for pc list and assign to the search bar
-    SearchHandler.assignSearchHandler(searchBar, listOfPCs);
-
-    // Set up dropdown
-    const svgs = document.getElementsByTagNameNS("http://www.w3.org/2000/svg", "svg");
-    const pcList = document.getElementById("List");
-    const list = [];
-    for (let i = 0; i < svgs.length; i++) {
-        list.push(svgs[i]);
-    }
-    list.push(pcList);
-
-    const dropdown = document.getElementById("labSelector") as HTMLSelectElement;
-    dropdown.addEventListener("change", () => {
-        for (let i = 0; i < list.length; i++) {
-            if (list[i].id === dropdown.value) {
-                list[i].classList.add("visible");
-            } else {
-                list[i].classList.remove("visible");
-            }
+        // Set up dropdown
+        const svgs = document.getElementsByTagNameNS("http://www.w3.org/2000/svg", "svg");
+        const pcList = document.getElementById("List");
+        const list = [];
+        for (let i = 0; i < svgs.length; i++) {
+            list.push(svgs[i]);
         }
-    });
+        list.push(pcList);
 
+        const dropdown = document.getElementById("labSelector") as HTMLSelectElement;
+        dropdown.addEventListener("change", () => {
+            for (let i = 0; i < list.length; i++) {
+                if (list[i].id === dropdown.value) {
+                    list[i].classList.add("visible");
+                } else {
+                    list[i].classList.remove("visible");
+                }
+            }
+        });
+    });
 };
 
 class StatusHandler {
@@ -67,8 +68,6 @@ class StatusHandler {
      */
     public updateStatuses = () => {
 
-        // @ts-ignore
-        const lab = document.getElementById(document.querySelector("svg.visible").id);
 
         // Loop through each lab machine in the PC list
         for (let i = 0; i < this.listOfPCs.length; i++) {
@@ -80,14 +79,11 @@ class StatusHandler {
 
             const id = "svg-" + tablePCAddress.innerHTML;
 
-            // @ts-ignore
-            const svgPCText = lab.getElementById(id);
-
+            const svgPCText = document.getElementById(id);
 
             // Update status each PC in the table
             StatusHandler.getStatus(tablePCAddress.innerHTML)
                 .then((foundUsers) => {
-
                     // PC is active
                     tablePC.classList.remove("inactive");
                     tablePC.classList.add("active");
@@ -283,42 +279,49 @@ class SearchHandler {
 class MapHandler {
 
     static applyCanvas(labRoom: LabRoom, roomSVG: SVGElement) {
-        const svgNS = "http://www.w3.org/2000/svg";
+        return new Promise((resolve, reject) => {
+            const svgNS = "http://www.w3.org/2000/svg";
 
-        roomSVG.style.color = labRoom.color;
-        roomSVG.style.width = labRoom.width.toString();
-        roomSVG.style.height = labRoom.height.toString();
-        roomSVG.setAttribute("name", labRoom.name);
+            roomSVG.style.color = labRoom.color;
+            roomSVG.style.width = labRoom.width.toString();
+            roomSVG.style.height = labRoom.height.toString();
+            roomSVG.setAttribute("name", labRoom.name);
 
-        const machines: LabMachineDetails[] = labRoom.machines;
+            const machines: LabMachineDetails[] = labRoom.machines;
 
-        for (let i = 0; i < machines.length; i++) {
-            const machine: LabMachineDetails = machines[i];
+            for (let i = 0; i < machines.length; i++) {
+                const machine: LabMachineDetails = machines[i];
 
-            const rect: SVGGraphicsElement = document.createElementNS(svgNS, "rect");
-            rect.setAttributeNS(null, 'x', String(machine.x));
-            rect.setAttributeNS(null, 'y', String(machine.y));
-            rect.setAttributeNS(null, 'height', String(machine.height));
-            rect.setAttributeNS(null, 'width', String(machine.width));
-            rect.setAttributeNS(null, 'fill', machine.color);
-            rect.setAttributeNS(null, 'stroke', "black");
-            rect.setAttributeNS(null, 'stroke-linecap', "square");
-            rect.setAttributeNS(null, 'stroke-opacity', "1");
-            rect.setAttributeNS(null, 'fill-opacity', "0.7");
+                const rect: SVGGraphicsElement = document.createElementNS(svgNS, "rect");
+                rect.setAttributeNS(null, 'x', String(machine.x));
+                rect.setAttributeNS(null, 'y', String(machine.y));
+                rect.setAttributeNS(null, 'height', String(machine.height));
+                rect.setAttributeNS(null, 'width', String(machine.width));
+                rect.setAttributeNS(null, 'fill', machine.color);
+                rect.setAttributeNS(null, 'stroke', "black");
+                rect.setAttributeNS(null, 'stroke-linecap', "square");
+                rect.setAttributeNS(null, 'stroke-opacity', "1");
+                rect.setAttributeNS(null, 'fill-opacity', "0.7");
 
-            const font: SVGTextElement = document.createElementNS(svgNS, "text");
-            font.setAttributeNS(null, 'x', String(machine.x + machine.width / 2));
-            font.setAttributeNS(null, 'y', String(machine.y + machine.height / 2));
-            font.setAttributeNS(null, 'text-anchor', "middle");
-            font.setAttributeNS(null, 'alignment-baseline', "central");
-            font.style.font = "10px sans-serif";
-            font.innerHTML = machine.name;
-            font.id = "svg-" + machine.name;
+                const font: SVGTextElement = document.createElementNS(svgNS, "text");
+                font.setAttributeNS(null, 'x', String(machine.x + machine.width / 2));
+                font.setAttributeNS(null, 'y', String(machine.y + machine.height / 2));
+                font.setAttributeNS(null, 'text-anchor', "middle");
+                font.setAttributeNS(null, 'alignment-baseline', "central");
+                font.style.font = "10px sans-serif";
+                font.innerHTML = machine.name;
+                font.id = "svg-" + machine.name;
 
-            roomSVG.appendChild(rect);
-            roomSVG.appendChild(font);
+                roomSVG.appendChild(rect);
+                roomSVG.appendChild(font);
 
-        }
+                if (i === machines.length - 1) {
+                    resolve();
+                    return;
+                }
+            }
+            reject();
+        })
 
     }
 
