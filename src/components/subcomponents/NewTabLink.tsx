e3d1dc7d78172fbf048f59dd08846bfa36a12ac0
Kathan Cheema <kathancheema@outlook.com>
2020-06-11 00:48:44 +0100

    Remove text decoration.

--- src/components/subcomponents/NewTabLink.tsx
+++ src/components/subcomponents/NewTabLink.tsx
@@ -3,7 +3,7 @@ import {NewTabLinkProps} from "../../common/types";
 
 export default function NewTabLink(props: NewTabLinkProps) {
     return <>
-        <a target="_blank" rel="noopener noreferrer" href={props.link}>
+        <a className={"no-a"} target="_blank" rel="noopener noreferrer" href={props.link}>
             {props.children}
         </a>
     </>;
