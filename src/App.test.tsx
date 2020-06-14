e33f459bc3e99f2d722219339e0e01e98ae4b221
Kathan Cheema <kathancheema@outlook.com>
2020-06-14 02:57:26 +0100

    Fix test suite.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,66 @@
 import React from 'react';
-import {render} from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import {cleanup, render, waitForElement} from '@testing-library/react';
 import App from './App';
-import './test/matchMedia.mock';
+import './test/scrollTo.mock';
+import {PageMapping} from "./common/types";
+import {isDesktop, isMobile, refreshMatchMedia} from "./test/matchMedia.mock";
 
-test('renders learn react link', () => {
-  const {getByText} = render(<App/>);
-// Comment while React.Suspense testing investigated. 
-//  const linkElement = getByText(/Home/i);
-//  expect(linkElement).toBeInTheDocument();
+afterEach(cleanup);
+beforeEach(refreshMatchMedia);
+
+test('given no modes are enabled, when the app loads, page headings and locations should be in the document', async () => {
+    const {getByText} = render(<App/>);
+    await waitForElement(() => getByText(/Home/));
+
+    PageMapping.forEach((locations, page) => {
+        expect(getByText(page)).toBeInTheDocument();
+        locations.locations.forEach(location => {
+            expect(getByText(location)).toBeInTheDocument();
+        })
+    });
 });
+
+test('given mobile mode is enabled, when the app loads, only page headings should be in the document', async () => {
+    isMobile();
+    const {getByText, queryByText} = render(<App/>);
+    await waitForElement(() => getByText(/Home/));
+
+    PageMapping.forEach((locations, page) => {
+        expect(getByText(page)).toBeInTheDocument();
+        if (page === 'Home') {
+            locations.locations.forEach(location => {
+                expect(getByText(location)).toBeInTheDocument();
+            })
+        } else {
+            locations.locations.forEach(location => {
+                expect(queryByText(location)).toBeNull();
+            })
+        }
+    });
+});
+
+test('given desktop mode is enabled, when the app loads, page headings and locations should be in the document', async () => {
+    isDesktop();
+    const {getByText} = render(<App/>);
+    await waitForElement(() => getByText(/Home/));
+
+    PageMapping.forEach((locations, page) => {
+        expect(getByText(page)).toBeInTheDocument();
+        locations.locations.forEach(location => {
+            expect(getByText(location)).toBeInTheDocument();
+        })
+    });
+});
+
+// Commented out until I figure out how to test light/dark mode
+//
+// test('given no modes are enabled, when the app loads to Home page, icons should be in light mode', async () => {
+//     isDarkMode();
+//     const {getByText} = render(<App/>);
+//     const linkedInLogo = await waitForElement(() => getByText(/LinkedIn Logo/));
+//
+//     expect(getByText(/#A0B6A5/)).toBeInTheDocument();
+//
+// });
+
--- src/test/matchMedia.mock.ts
+++ src/test/matchMedia.mock.ts
@@ -1,14 +1,44 @@
 // @ts-ignore
-Object.defineProperty(window, 'matchMedia', {
-    writable: true,
-    value: jest.fn().mockImplementation(query => ({
-        matches: false,
-        media: query,
-        onchange: null,
-        addListener: jest.fn(), // deprecated
-        removeListener: jest.fn(), // deprecated
-        addEventListener: jest.fn(),
-        removeEventListener: jest.fn(),
-        dispatchEvent: jest.fn(),
-    })),
+
+let currentMatches: Map<string, boolean> = new Map<string, boolean>([]);
+
+export function refreshMatchMedia() {
+    currentMatches.clear();
+    applyMatchMedia();
+}
+
+export function isDesktop() {
+    currentMatches.set('(max-width: 750px)', false);
+    applyMatchMedia();
+}
+
+export function isMobile() {
+    currentMatches.set('(max-width: 750px)', true);
+    applyMatchMedia();
+}
+
+export function isLightMode() {
+    currentMatches.set('(prefers-color-scheme: dark)', false);
+    applyMatchMedia();
+}
+
+export function isDarkMode() {
+    currentMatches.set('(prefers-color-scheme: dark)', true);
+    applyMatchMedia();
+}
+
+function applyMatchMedia() {
+    Object.defineProperty(window, 'matchMedia', {
+        writable: true,
+        value: jest.fn().mockImplementation(query => ({
+            matches: currentMatches.get(query),
+            media: query,
+            onchange: null,
+            addListener: jest.fn(), // deprecated
+            removeListener: jest.fn(), // deprecated
+            addEventListener: jest.fn(),
+            removeEventListener: jest.fn(),
+            dispatchEvent: jest.fn(),
+        })),
 });
+}
